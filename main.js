06ae9941cf8bde26764727e583278b4b5a06aa69
JRudransh <JRudransh@protonmail.com>
2022-03-14 19:56:16 +0530

    JS Class

---
+++ main.js
@@ -0,0 +1,58 @@
+var v1 = 'User1'
+let v2 = 'user2'
+
+
+const v3 = 'user3'
+
+
+// Datatypes
+// Primitive
+
+// numbers
+n1 = 10
+n2 = 10.0
+n3 = 0xF
+n4 = 5.1e4
+n5 = NaN
+// 5.1*10^4
+
+// string
+s1 = 'hello'
+s2 = "world"
+s3 = `${s1} JavaScript`
+s4 = s1 + ' ' + s2
+s5 = 'Hello\n \
+\tWorld'
+s6 = '10'
+
+// boolean
+b1 = true
+b2 = false
+
+// null
+n_1 = null;
+
+// undefined
+let n_;
+
+// Non Primitive
+
+// array
+arr1 = [1,2,3,4,'Hello', -200, n1]
+
+// boject
+obj1 = {
+    name: "jay",
+    id: 200,
+    class: 10,
+    arr1,
+}
+
+// function
+let f1 = () => {
+    console.log('Hello Function');
+}
+
+// Operator
+// == same value
+// === same value and type
